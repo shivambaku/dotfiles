b5853e976028d0d7f88bb9dd2d3840f380b40d57
Shivam Kumar <shivambaku@gmail.com>
2023-08-23 02:17:39 +0400

    .

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+# MacOs settings
+defaults write -g ApplePressAndHoldEnabled -bool false
+
 # Setup syslinks
 ln -sfv ~/dotfiles/.config/nvim ~/.config/nvim
 ln -sfv ~/dotfiles/.zshrc ~/.zshrc
+ln -sfv ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
