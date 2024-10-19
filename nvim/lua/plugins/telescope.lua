785732801869917df99fd6929e2cbf29932ef61f
Shivam Kumar <shivambaku@gmail.com>
2024-10-19 15:21:36 -0700

    feat: Initial setup for treesitter

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,5 +1,6 @@
 return {
-  'nvim-telescope/telescope.nvim', tag = '0.1.8',
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.8',
   dependencies = { 'nvim-lua/plenary.nvim' },
   opts = function()
     local actions = require("telescope.actions")
