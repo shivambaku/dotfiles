8cc46c1b6363fd4b7f08d3a4e6a9d54a9d417843
Shivam Kumar <shivambaku@gmail.com>
2024-10-20 18:42:30 -0700

    chore: Improved telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,9 +1,14 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	tag = "0.1.8",
-	dependencies = { "nvim-lua/plenary.nvim" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		"nvim-telescope/telescope-live-grep-args.nvim",
+	},
 	config = function()
-		require("telescope").setup({
+		local telescope = require("telescope")
+		telescope.setup({
 			defaults = {
 				mappings = {
 					i = {
@@ -12,13 +17,18 @@ return {
 				},
 			},
 		})
+
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
 		vim.keymap.set("n", "<leader>fp", builtin.git_files, { desc = "Find files (excluding git ingores)" })
-		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
 		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Find help" })
 		vim.keymap.set("n", "<leader>fr", builtin.lsp_references, { desc = "Find references" })
 		vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, { desc = "Find symbols" })
 		vim.keymap.set("n", "<leader>fx", builtin.diagnostics, { desc = "Diagnostics" })
+
+		telescope.load_extension("live_grep_args")
+		vim.keymap.set("n", "<leader>fg", function()
+			telescope.extensions.live_grep_args.live_grep_args()
+		end, { desc = "Live grep" })
 	end,
 }
