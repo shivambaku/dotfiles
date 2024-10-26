d064d356ad5f52f5b4abca21d6e014ddb114fca4
Shivam Kumar <shivambaku@gmail.com>
2024-10-25 21:41:30 -0700

    feat: Lazy loading and fix treesitter priority

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	version = false,
+	event = { "BufReadPre", "BufNewFile" },
 	build = ":TSUpdate",
 	config = function()
 		require("nvim-treesitter.configs").setup({
@@ -15,5 +16,8 @@ return {
 				additional_vim_regex_highlighting = false,
 			},
 		})
+
+		-- Lowering the LSP priority so treesitter has higher priority.
+		vim.highlight.priorities.semantic_tokens = 95
 	end,
 }
