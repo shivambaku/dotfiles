bff98985ff2a526ab0e6606836c5033b4e1564e1
Shivam Kumar <shivambaku@gmail.com>
2024-12-24 23:26:28 -0800

    chore

--- nvim/lua/plugins/multicursor.lua
+++ nvim/lua/plugins/multicursor.lua
@@ -4,9 +4,9 @@ return {
 	config = function()
 		vim.g.VM_silent_exit = true
 		vim.g.VM_quit_after_leaving_insert_mode = true
+		vim.g.VM_default_mappings = 0
+
 		vim.api.nvim_set_hl(0, "VM_Extend", { bg = "#3d59a1", fg = "#c0caf5" })
 		vim.api.nvim_set_hl(0, "VM_Cursor", { bg = "#3d59a1", fg = "#c0caf5" })
-		-- vim.api.nvim_set_hl(0, "VM_Extend", { link = "Search" })
-		-- vim.api.nvim_set_hl(0, "VM_Cursor", { link = "Search" })
 	end,
 }
