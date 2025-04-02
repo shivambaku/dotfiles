214364600af87ce4dfa50f4123f579b4e176ef93
Shivam Kumar <shivambaku@gmail.com>
2025-04-01 23:01:59 -0700

    feat: Zeta testing

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -4,15 +4,24 @@ return {
 	event = { "InsertEnter", "CmdlineEnter" },
 	config = function()
 		require("blink.cmp").setup({
-			keymap = { preset = "super-tab" },
+			keymap = {
+				preset = "super-tab",
+				["<A-y>"] = require("minuet").make_blink_map(),
+			},
 			sources = {
-				default = { "lsp", "buffer", "snippets", "path" },
+				default = { "lsp", "buffer", "snippets", "path", "minuet" },
 				per_filetype = { sql = { "dadbod" } },
 				providers = {
 					dadbod = { module = "vim_dadbod_completion.blink" },
+					minuet = {
+						name = "minuet",
+						module = "minuet.blink",
+						score_offset = 8,
+					},
 				},
 			},
 			fuzzy = { implementation = "rust" },
+			completion = { trigger = { prefetch_on_insert = false } },
 		})
 	end,
 }
