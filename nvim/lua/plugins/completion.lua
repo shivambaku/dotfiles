f0065dc20be5c5996c3de1f8f4615b718389c854
Shivam Kumar <shivambaku@gmail.com>
2025-04-01 23:02:51 -0700

    feat: Removed zeta for now

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -4,24 +4,15 @@ return {
 	event = { "InsertEnter", "CmdlineEnter" },
 	config = function()
 		require("blink.cmp").setup({
-			keymap = {
-				preset = "super-tab",
-				["<A-y>"] = require("minuet").make_blink_map(),
-			},
+			keymap = { preset = "super-tab" },
 			sources = {
-				default = { "lsp", "buffer", "snippets", "path", "minuet" },
+				default = { "lsp", "buffer", "snippets", "path" },
 				per_filetype = { sql = { "dadbod" } },
 				providers = {
 					dadbod = { module = "vim_dadbod_completion.blink" },
-					minuet = {
-						name = "minuet",
-						module = "minuet.blink",
-						score_offset = 8,
-					},
 				},
 			},
 			fuzzy = { implementation = "rust" },
-			completion = { trigger = { prefetch_on_insert = false } },
 		})
 	end,
 }
