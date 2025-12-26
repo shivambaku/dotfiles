891343e4b8195bb8c7599cbbe03f873e70ea3b0c
Shivam Kumar <shivambaku@gmail.com>
2025-12-26 12:59:08 -0800

    chore: nvim-treesitter main

--- stow/common/.config/nvim/lua/plugins/treesitter.lua
+++ stow/common/.config/nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	version = false,
-	event = { "BufReadPre", "BufNewFile" },
+	branch = "main",
+	lazy = false,
 	build = ":TSUpdate",
 	dependencies = {
 		{
@@ -10,15 +10,6 @@ return {
 		},
 	},
 	config = function()
-		require("nvim-treesitter.configs").setup({
-			sync_install = false,
-			auto_install = true,
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-		})
-
 		-- Lowering the LSP priority so treesitter has higher priority.
 		vim.highlight.priorities.semantic_tokens = 95
 	end,
