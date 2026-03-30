863b265b95ca8db38d0a11df7190262b213d922a
Shivam Kumar <shivambaku@gmail.com>
2026-03-29 20:45:11 -0700

    fix: treesitter

--- common/.config/nvim/lua/plugins/treesitter.lua
+++ common/.config/nvim/lua/plugins/treesitter.lua
@@ -4,12 +4,41 @@ return {
 	lazy = false,
 	build = ":TSUpdate",
 	dependencies = {
-		{
-			"JoosepAlviste/nvim-ts-context-commentstring",
-			lazy = true,
-		},
+		-- {
+		-- 	"JoosepAlviste/nvim-ts-context-commentstring",
+		-- 	lazy = true,
+		-- },
 	},
 	config = function()
+		require("nvim-treesitter").install({
+			"bash",
+			"c",
+			"cpp",
+			"gitcommit",
+			"go",
+			"graphql",
+			"html",
+			"java",
+			"javascript",
+			"json",
+			"json5",
+			"jsonc",
+			"lua",
+			"markdown",
+			"markdown_inline",
+			"python",
+			"query",
+			"regex",
+			"rust",
+			"scss",
+			"toml",
+			"tsx",
+			"typescript",
+			"vim",
+			"vimdoc",
+			"yaml",
+		})
+
 		-- Lowering the LSP priority so treesitter has higher priority.
 		vim.highlight.priorities.semantic_tokens = 95
 	end,
