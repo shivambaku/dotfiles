506c233ab46437bc0bf4c29b9f1a795f23da659e
Shivam Kumar <shivambaku@gmail.com>
2026-05-11 19:56:14 -0700

    fix: lazy git panel

--- common/.config/wezterm/lazygit.lua
+++ common/.config/wezterm/lazygit.lua
@@ -4,15 +4,20 @@ local utils_session = require("utils-session")
 local module = {}
 
 function module.open()
-	return wezterm.action_callback(function(_, pane)
+	return wezterm.action_callback(function(window, pane)
 		local cwd = utils_session.current_path(pane)
-		local lazygit_pane = pane:split({
-			direction = "Bottom",
-			top_level = true,
-			size = 0.85,
-			cwd = cwd,
-			args = { "/bin/zsh", "-lc", "lazygit" },
-		})
+		local ok, lazygit_pane = pcall(function()
+			return pane:split({
+				direction = "Bottom",
+				size = 0.85,
+				cwd = cwd,
+				args = { "/bin/zsh", "-lc", "lazygit" },
+			})
+		end)
+		if not ok or not lazygit_pane then
+			utils_session.notify("Lazygit", window, "Unable to open lazygit pane", "warn")
+			return
+		end
 
 		lazygit_pane:activate()
 		lazygit_pane:tab():set_zoomed(true)
